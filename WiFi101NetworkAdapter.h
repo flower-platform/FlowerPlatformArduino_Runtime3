336b9bdf49085fc7998a2996015eb0213e544a84
Claudiu Matei <claudiu@shmessenger.org>
2016-05-24 10:41:58 +0300

    WiFi101 network adapter

---
+++ WiFi101NetworkAdapter.h
@@ -0,0 +1,42 @@
+/*
+ *  Author: Claudiu Matei
+ */
+
+#ifndef WiFi101NetworkAdapter_h
+#define WiFi101NetworkAdapter_h
+
+#include <IProtocolHandler.h>
+#include <IWiFiNetworkAdapter.h>
+#include <SPI.h>
+#include <WiFi101.h>
+
+class WiFi101NetworkAdapter : public IWiFiNetworkAdapter {
+protected:
+
+	WiFiServer* server = NULL;
+
+public:
+
+	void setup();
+
+	void loop();
+
+};
+
+void WiFi101NetworkAdapter::setup() {
+	this->server = new WiFiServer(protocolHandler->port);
+	WiFi.config(ipAddress);
+	WiFi.begin(ssid, password);
+	this->server->begin();
+}
+
+void WiFi101NetworkAdapter::loop() {
+	// listen for incoming clients
+	WiFiClient client = server->available();
+	if (client) {
+		server->begin();
+		protocolHandler->processClientRequest(&client);
+	}
+}
+
+#endif
