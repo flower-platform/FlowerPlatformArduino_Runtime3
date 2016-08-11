66e79051c60643754af7d66fc355a2b62b75aa0e
ancab <anca_denisa.barbu@cti.pub.ro>
2016-08-11 15:07:13 +0300

    String manipulation

--- FlowerPlatformStringManip.h
+++ FlowerPlatformStringManip.h
@@ -1,10 +1,16 @@
-String changeCaseOfString(String s, char* caseMode) {
+/*
+ *  Author: Anca Barbu
+ */
+String changeCaseOfString(String s, String caseMode) {
 	String result = s;
-	if (strcmp(caseMode, "lower")) {
+	
+	if (caseMode == "lower") {
 		result.toLowerCase();
-	} else if (strcmp(caseMode, "upper")) {
+	} else if (caseMode == "upper") {
 		result.toUpperCase();
 	}
+	Serial.println(s);
+	Serial.println(result);
 	return result;
 }
 
@@ -19,3 +25,36 @@ String getReplacedString(String text, String what, String with) {
 	result.replace(what, with);
 	return result;
 }
+
+//TODO when we add suport for lists, it needs to be modified
+/*String* splitText(String text, String delim) {
+	String copyText = text;
+	int c, found = 0;
+	while ( (c = copyText.indexOf(delim)) != -1) {
+		found ++;
+		copyText = copyText.substring(c + delim.length()	, copyText.length());
+	}
+	String result[found];
+	int pos, i = 0;
+	copyText = text; 
+	
+	for (i = 0; i < found; i++) {
+		pos = copyText.indexOf(delim);
+		result[i] = copyText.substring(0, pos);
+		copyText = copyText.substring(pos + delim.length(), copyText.length());		
+	}
+	result[found] = copyText;
+	return result;
+}
+
+//TODO when we add suport for lists, it needs to be modified
+String* splitTextAtFirst(String text, String delim) {
+	String result[2];
+	int c;
+	if ( (c = text.indexOf(delim)) != -1) {
+		result[0] = text.substring(0, c);
+		result[1] = text.substring(c + delim.length(), text.length());
+	}
+	return result;
+
+}*/
