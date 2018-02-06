27d90781bdd9c94eeee19ce0fa6ad0cc5c56cc4b
Claudiu Matei <claudiu.matei@shmessenger.org>
2018-02-06 14:03:10 +0200

    RemoteObject: self pointer in callback

--- RemoteObject.h
+++ RemoteObject.h
@@ -22,9 +22,9 @@
 #define DEFAULT_BUFFER_SIZE 128
 
 extern bool dispatchFunctionCall(char* functionCall, Print* response);
-extern void registerCallback(uint16_t callbackId, void* callback, uint8_t returnTypeId);
-extern bool executeCallback(uint16_t callbackId, Stream *response);
-bool executeCallback(void* callback, uint8_t returnType, Stream *response);
+void registerCallback(uint16_t callbackId, void* self, void* callback, uint8_t returnTypeId);
+bool executeCallback(uint16_t callbackId, Stream *response);
+bool executeCallback(void* self, void* callback, uint8_t returnType, Stream *response);
 
 class RemoteObject {
 public:
@@ -45,13 +45,13 @@ protected:
 
 	const char* securityToken;
 
-	bool callFunction(const char* functionNamePSTR, SmartBuffer<>* argsBuf, void *callback, uint8_t returnTypeId);
+	bool callFunction(const char* functionNamePSTR, SmartBuffer<>* argsBuf, void *callback, uint8_t returnTypeId, void* self);
 
 	virtual Stream* sendRequest(SmartBuffer<DEFAULT_BUFFER_SIZE>* buf, SmartBuffer<>* argsBuf) = 0;
 
 };
 
-bool RemoteObject::callFunction(const char* functionNamePSTR, SmartBuffer<>* argsBuf, void *callback = NULL, uint8_t returnTypeId = TYPE_VOID) {
+bool RemoteObject::callFunction(const char* functionNamePSTR, SmartBuffer<>* argsBuf, void *callback = NULL, uint8_t returnTypeId = TYPE_VOID, void* self = NULL) {
 	SmartBuffer<DEFAULT_BUFFER_SIZE> buf;
 	fprp_startPacket(&buf, 'I', securityToken);
 	if (rappInstance) {
@@ -91,43 +91,79 @@ bool RemoteObject::callFunction(const char* functionNamePSTR, SmartBuffer<>* arg
 	switch(cmd) {
 	case 'R': // result
 		in->readBytesUntil(TERM, callbackIdStr, 8); // callbackId (ignored)
-		executeCallback(callback, returnTypeId, in);
+		executeCallback(self, callback, returnTypeId, in);
 		break;
 	case 'P': // pending
 		int size = in->readBytesUntil(TERM, callbackIdStr, 8); // callbackId
 		callbackIdStr[size] = TERM;
-		registerCallback((uint16_t) atol(callbackIdStr), callback, returnTypeId);
+		registerCallback((uint16_t) atol(callbackIdStr), self, callback, returnTypeId);
 		break;
 	}
 
 	return true;
 }
 
-bool executeCallback(void* callback, uint8_t returnType, Stream *response) {
+
+struct RemoteObjectCallback {
+	uint16_t callbackId;
+	void* callbackFunction;
+	void* selfObject;
+	uint8_t returnType;
+} callbacks[MAX_CALLBACKS];
+
+uint8_t callbackIndex = 0;
+
+void registerCallback(uint16_t callbackId, void* self, void* callback, uint8_t returnTypeId) {
+	RemoteObjectCallback cb = callbacks[callbackIndex];
+	cb.callbackId = callbackId;
+	cb.callbackFunction = callback;
+	cb.selfObject = self;
+	cb.returnType = returnTypeId;
+	callbackIndex++;
+	if (callbackIndex >= MAX_CALLBACKS) {
+		callbackIndex = 0;
+	}
+}
+
+bool executeCallback(uint16_t callbackId, Stream *response) {
+	uint8_t cbIndex = 0;
+	while (cbIndex < MAX_CALLBACKS && (callbacks[cbIndex].callbackId != callbackId ) ) {
+		cbIndex++;
+	}
+	if (cbIndex == MAX_CALLBACKS) {
+		return false;
+	}
+	RemoteObjectCallback cb = callbacks[callbackIndex];
+	void* callbackFunction = cb.callbackFunction;
+	cb.callbackFunction = NULL;
+	return executeCallback(cb.selfObject, callbackFunction, cb.returnType, response);
+}
+
+bool executeCallback(void* self, void* callback, uint8_t returnType, Stream *response) {
 	int size;
 	switch (returnType) {
 	case TYPE_VOID:
-		((void (*)()) callback)();
+		((void (*)(void*)) callback)(self);
 		break;
 	case TYPE_INT: {
 		char valueStr[8];
 		int size = response->readBytesUntil(EOT, valueStr, 8);
 		valueStr[size] = TERM;
 		int value = atoi(valueStr);
-		((void (*)(int)) callback)(value);
+		((void (*)(void*, int)) callback)(self, value);
 	} break;
 	case TYPE_BOOL: {
 		char valueStr[8];
 		size = response->readBytesUntil(EOT, valueStr, 8);
 		valueStr[size] = TERM;
 		int value = atoi(valueStr);
-		((void (*)(bool)) callback)(value);
+		((void (*)(void*, bool)) callback)(self, value);
 	} break;
 	case TYPE_STRING: {
 		char value[65];
 		int size = response->readBytesUntil(EOT, value, 64);
 		value[size] = TERM;
-		((void (*)(const char*)) callback)(value);
+		((void (*)(void*, const char*)) callback)(self, value);
 	} break;
 	}
 	return true;
